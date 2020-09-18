f5324fd4187cc72e104762bb1cfa3996ed90c334
Mark LeMoine <linclelinkpart5@gmail.com>
2020-09-17 23:51:15 -0700

    Added processing function, and updated example files

--- process.py
+++ process.py
@@ -23,11 +23,11 @@ def get_arg_parser():
         type=pl.Path,
         help='Input dir path to read FLAC files from',
     )
-    parser.add_argument(
-        'output_dir',
-        type=pl.Path,
-        help='Output dir path to write modified FLAC files to',
-    )
+    # parser.add_argument(
+    #     'output_dir',
+    #     type=pl.Path,
+    #     help='Output dir path to write modified FLAC files to',
+    # )
     parser.add_argument(
         'album_file',
         type=pl.Path,
@@ -37,6 +37,11 @@ def get_arg_parser():
         'track_file',
         type=pl.Path,
         help='Path to HJSON file defining track-level fields',
+    )
+    parser.add_argument(
+        '--intermediate',
+        action='store_true',
+        help='Show intermediate information, e.g. artist/title from source files',
     )
 
     return parser
@@ -49,19 +54,20 @@ def collect_entries(source_dir: pl.Path) -> tp.List[Entry]:
     expected_track_nums = set(i + 1 for i in range(len(src_paths)))
 
     for src_path in src_paths:
-        with src_path.open(mode='rb') as fp:
-            flac_data = mutagen.flac.FLAC(fp)
-            tags = flac_data.tags
+        print(f'Reading existing tags from {src_path.name}')
+        flac_data = mutagen.flac.FLAC(src_path)
 
-            assert 'TRACKNUMBER' in tags
-            tn = tags['TRACKNUMBER']
-            assert isinstance(tn, list)
-            assert len(tn) == 1
-            track_num = int(tn[0])
+        tags = flac_data.tags
 
-            expected_track_nums.remove(track_num)
+        assert 'TRACKNUMBER' in tags
+        tn = tags['TRACKNUMBER']
+        assert isinstance(tn, list)
+        assert len(tn) == 1
+        track_num = int(tn[0])
 
-            entries.append(Entry(path=src_path, tags=tags, track_num=track_num))
+        expected_track_nums.remove(track_num)
+
+        entries.append(Entry(path=src_path, tags=tags, track_num=track_num))
 
     assert len(expected_track_nums) == 0
 
@@ -111,12 +117,45 @@ def load_track_blocks(path: pl.Path) -> tp.List[Block]:
 
     assert isinstance(track_block_candidates, list)
 
-    for i in range(len(track_block_candidates)):
-        track_block_candidates[i] = normalize_block_candidate(track_block_candidates[i])
-
-    track_blocks = track_block_candidates
+    track_blocks = []
+    for track_block_candidate in track_block_candidates:
+        track_blocks.append(normalize_block_candidate(track_block_candidate))
 
     return track_blocks
+
+
+def process_entries(
+    entries: tp.Sequence[Entry],
+    album_block: Block,
+    track_blocks: tp.Sequence[Block],
+):
+    # Check that there are equal numbers of track blocks and entries.
+    assert(len(track_blocks) == len(entries))
+
+    num_total_tracks = len(track_blocks)
+
+    for entry, track_block in zip(entries, track_blocks):
+        flac_data = mutagen.flac.FLAC(entry.path)
+
+        # Remove all existing tags.
+        flac_data.tags.clear()
+
+        # Remove any pictures.
+        flac_data.clear_pictures()
+
+        # Add in album block fields.
+        flac_data.tags.update(album_block)
+
+        # Add in the track block fields for this entry.
+        flac_data.tags.update(track_block)
+
+        # Add track index/count info to tags.
+        flac_data.tags['TRACKNUMBER'] = [str(entry.track_num)]
+        flac_data.tags['TOTALTRACKS'] = [str(num_total_tracks)]
+
+        print(flac_data.pprint())
+
+        flac_data.save()
 
 
 if __name__ == '__main__':
@@ -126,22 +165,27 @@ if __name__ == '__main__':
     source_dir = args.source_dir
     album_file = args.album_file
     track_file = args.track_file
+    show_inter = args.intermediate
 
     entries = collect_entries(source_dir)
 
-    # Output intermediate data and pause for user input.
-    # We care about artist and title info.
-    intermediates = []
-    for entry in entries:
-        sub_tags = {k: entry.tags[k] for k in ('ARTIST', 'TITLE')}
-        intermediates.append(sub_tags)
+    if show_inter:
+        # Output intermediate data and pause for user input.
+        # We care about artist and title info.
+        intermediates = []
+        for entry in entries:
+            sub_tags = {k: entry.tags[k] for k in ('ARTIST', 'TITLE')}
+            intermediates.append(sub_tags)
 
-    print(hjson.dumps(intermediates))
+        print(hjson.dumps(intermediates))
 
-    input("Press Enter to continue...")
+        input("Press Enter to continue...")
 
     album_block = load_album_block(album_file)
     track_blocks = load_track_blocks(track_file)
 
-    # Check that there are equal numbers of track blocks and FLAC files.
-    assert(len(track_blocks) == len(entries))
+    process_entries(
+        entries=entries,
+        album_block=album_block,
+        track_blocks=track_blocks,
+    )
